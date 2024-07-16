780482ba0dec6d4ec1f371fbe8cabcf4c434cf1c
jin2304 <search2304@naver.com>
2024-07-16 11:21:05 +0900

    feat :회원가입 기능 추가
    - JoinController 추가
    - JoinService 추가
      - joinProcess(), 회원가입 메서드 추가
       -사용자 이름 중복 확인
       -비밀번호 암호화
    - UserRepository에서 existsByUsername() 메서드 추가
    - JoinDTO 추가

--- src/main/java/com/example/jwt_project/repository/UserRepository.java
+++ src/main/java/com/example/jwt_project/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.example.jwt_project.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    //사용자 이름 붕복 확인
+    Boolean existsByUsername(String username);
 }
